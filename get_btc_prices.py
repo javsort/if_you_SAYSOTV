42b83fd5769d0c2cf4ca39283b4c3b6138197ef1
javsort <javierortega336@gmail.com>
2026-02-14 16:44:54 +0100

    formula works. Enhancements & graphics upcoming

--- get_btc_prices.py
+++ get_btc_prices.py
@@ -1,29 +1,20 @@
 import json
-import csv
 from yfinance import Ticker
 from datetime import datetime, timedelta
 
+def get_purchase_data(purchase_data_path):
+    json_data = {}
 
-def get_fast_info(btc_ticker, keys_to_ret):
+    with open(purchase_data_path) as json_file:
+        json_data = json.load(json_file)
      
-    fast_info = btc_ticker.get_fast_info()
+    return json_data
 
-    #print(f"BTC Info: {json.dumps(, indent=4)}")
-    print(f"Fast info: {fast_info}")
 
-    for key in keys_to_ret:
-        if key in fast_info:
-            current_element = fast_info[key]
-            print(f"For key '{key}':\n{current_element}\n---------------")
-            
-        last_price = fast_info['lastPrice']
-     
-def get_price_on_dates(btc_ticker, dates_to_check):
-    dates_n_prices = {}
+def get_price_on_dates(btc_ticker, dates_to_check, purchase_data):
 
     for x in dates_to_check:
         day_4_ticker = btc_ticker.history(start=x, end=next_day(x), interval="1d")
-        #end_price = float(day_4_ticker["Close"].iloc[0])
         if day_4_ticker.empty:
             print(f"Found nothing for: {x}")
             continue
@@ -33,48 +24,92 @@ def get_price_on_dates(btc_ticker, dates_to_check):
         close_price = float(day_4_ticker["Close"].iloc[0])
         #print(f"Close price: {close_price}")
 
-        if x not in dates_n_prices:
-             dates_n_prices[x] = close_price
+        if x in purchase_data:
+            purchase_data[x]["close_price"] = close_price
      
-    return dates_n_prices
+    return purchase_data
 
-def get_purchase_data(dates_json_path):
-    dates = []
-
-    with open(dates_csv_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-
-        for row in csv_reader:
-            current_date = row[0]
-            dates.append(current_date)
-     
-    return dates
-
-def main():
-
-    btc_ticker = Ticker("BTC-EUR")
-    
-    keys_to_ret = ['currency', 'dayHigh', 'dayLow', 'exchange', 'fiftyDayAverage', 'lastPrice', 'lastVolume', 
-                   'marketCap', 'open', 'previousClose', 'quoteType', 'regularMarketPreviousClose', 'shares', 
-                   'tenDayAverageVolume', 'threeMonthAverageVolume', 'timezone', 'twoHundredDayAverage', 'yearChange', 
-                   'yearHigh', 'yearLow']
-    
-    purchase_data_path = "purchase_data.json"
-
-    purchase_data = get_purchase_data(purchase_data_path)
-
-    #get_fast_info(btc_ticker=btc_ticker, keys_to_ret=keys_to_ret)
-
-    dates_to_check = purchase_data.keys()
-    dates_n_prices = get_price_on_dates(btc_ticker=btc_ticker, dates_to_check=dates_to_check)
-
-
-    print(f"Overall findings: \n{json.dumps(dates_n_prices, indent=4)}")
-    return
 
 def next_day(date_str: str) -> str:
     d = datetime.strptime(date_str, "%Y-%m-%d")
     return (d + timedelta(days=1)).strftime("%Y-%m-%d")
+
+
+def get_fast_info(btc_ticker, keys_to_ret):
+     
+    fast_info = btc_ticker.get_fast_info()
+    last_price = fast_info['lastPrice']
+
+    #print(f"BTC Info: {json.dumps(, indent=4)}")
+    #print(f"Fast info: {fast_info}")
+    # for key in keys_to_ret:
+    #     if key in fast_info:
+    #         current_element = fast_info[key]
+    #         print(f"For key '{key}':\n{current_element}\n---------------")
+
+    return last_price
+
+
+def get_todays_comp(dates_n_prices, last_price):
+    """
+    Purchase value - P_val      -     Bought at close price - C_price
+    Current value -  T_val      -     Today's Price - T_price
+    
+    Formula: X = (P_val * T_price) / C_price
+    """
+    full_json = {}
+    
+    T_price = last_price
+    for date, content in dates_n_prices.items():
+        P_val = float(content["purchase_value"])
+        C_price = content["close_price"]
+
+        T_val = (P_val * T_price) / C_price
+
+        print(f"For date: {date}\n  - close_price: {C_price}  -  purchase_value: {P_val}\n  - last_price: '{T_price}'  -  current_value: {T_val}\n")
+
+        dates_n_prices[date]["current_value"] = T_val
+
+    full_json = dates_n_prices.copy()
+    return full_json
+
+
+def main():
+    btc_ticker = Ticker("BTC-EUR")
+    keys_to_ret = ['currency', 'dayHigh', 'dayLow', 'exchange', 'fiftyDayAverage', 
+                   'lastPrice', 'lastVolume', 'marketCap', 'open', 'previousClose', 
+                   'quoteType', 'regularMarketPreviousClose', 'shares', 'tenDayAverageVolume', 
+                   'threeMonthAverageVolume', 'timezone', 'twoHundredDayAverage', 'yearChange', 
+                   'yearHigh', 'yearLow']
+    
+    """
+    # Open up the json file with the purchase info
+    # Format of json:
+    # {
+    #     "YYYY-MM-DD": {
+    #         "purchase_value": 0,
+    #         "current_value": 0,
+    #         "btc_quantity": 0,
+    #         "close_price": 0
+    #     },
+    #     ...
+    # }
+    """
+    purchase_data_path = "purchase_data.json"
+
+    purchase_data = get_purchase_data(purchase_data_path=purchase_data_path)
+
+    dates_to_check = purchase_data.keys()
+    dates_n_prices = get_price_on_dates(btc_ticker=btc_ticker, dates_to_check=dates_to_check, purchase_data=purchase_data)
+
+
+    # Now get the current price:
+    last_price = get_fast_info(btc_ticker=btc_ticker, keys_to_ret=keys_to_ret)
+
+    up_to_date = get_todays_comp(dates_n_prices=dates_n_prices, last_price=last_price)
+    print(f"Overall findings: \n{json.dumps(up_to_date, indent=4)}")
+    
+    return
 
 if __name__ == "__main__":
         main()
